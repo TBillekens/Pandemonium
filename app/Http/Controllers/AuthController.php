9d47cddaaf957a9361558c34d566eea0bd3a79c8
Timmy Billekens <timmybillekens@gmail.com>
2024-07-02 12:47:00 +0200

    feat: created register, login and logout routes and functionality also removed the welcome view

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $user = User::create($data);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return [
+            'user' => $user,
+            'token' => $token,
+        ];
+    }
+
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $data['email'])->first();
+
+        if (!$user || !Hash::check($data['password'], $user->password)) {
+            return response([
+                'message' => 'Bad credentials',
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken; 
+
+        return [
+            'user' => $user,
+            'token' => $token,
+        ];
+    }
+
+    public function logout(Request $request)
+    {
+        $data = $request->validate([
+            'token' => 'required|string',
+            'email' => 'required|email',
+        ]);
+
+        $user = User::where('email', $data['email'])->first();
+
+        try {
+            $user->tokens()->where('id', $data['token'])->delete();
+        } catch (\Throwable $th) {
+            return response([
+                'message' => 'Bad credentials',
+            ], 401);
+        }
+
+        return [
+            'message' => 'Logged out',
+            'user' => $user,
+        ];
+    }
+}
