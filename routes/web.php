cb47b3e33592da09f8fe2dffe7340230bb2dfc9a
Timmy Billekens <timmybillekens@gmail.com>
2024-07-06 16:13:06 +0200

    feat: changed routes for openlibrary

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\LibraryController;
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\OpenLibraryController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
@@ -12,17 +13,18 @@ Route::get('/', function (Request $request) {
 })->name('dashboard');
 
 Route::group([
-        'prefix' => 'auth',
-    ], function () {
-        Route::post('/register', AuthController::class.'@register')->name('register');
-        Route::post('/login', AuthController::class.'@login')->name('login');
-        Route::post('/logout', AuthController::class.'@logout')->name('logout');
+    'prefix' => 'auth',
+], function () {
+    Route::post('/register', AuthController::class . '@register')->name('register');
+    Route::post('/login', AuthController::class . '@login')->name('login');
+    Route::post('/logout', AuthController::class . '@logout')->name('logout');
 });
 
 Route::group([
-        'middleware' => 'auth',
-    ], function () {
-        Route::resource('library', LibraryController::class);
-        Route::get('openlibrary/search', OpenLibraryController::class.'@search')->name('openlibrary.search');
-
+    'middleware' => 'auth',
+], function () {
+    Route::resource('library', LibraryController::class);
+    Route::resource('library.books', BookController::class);
+    Route::get('/openlibrary/', OpenLibraryController::class . '@index')->name('openlibrary.index');
+    Route::post('/openlibrary/search', OpenLibraryController::class . '@search')->name('openlibrary.search');
 });
