1dca7fcd54d403564a47bd3bfacd19a2d0c0650f
Timmy Billekens <timmybillekens@gmail.com>
2024-07-07 03:13:33 +0200

    feat: added new routes and fixed problem for unlogged in redirect

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ Route::get('/', function (Request $request) {
 Route::group([
     'prefix' => 'auth',
 ], function () {
+    Route::get('/register', function () {return redirect()->route('dashboard');})->name('register');
     Route::post('/register', AuthController::class . '@register')->name('register');
+    Route::get('/login', function () {return redirect()->route('dashboard');})->name('login');
     Route::post('/login', AuthController::class . '@login')->name('login');
     Route::post('/logout', AuthController::class . '@logout')->name('logout');
 });
@@ -24,7 +26,7 @@ Route::group([
     'middleware' => 'auth',
 ], function () {
     Route::resource('library', LibraryController::class);
-    Route::resource('library.books', BookController::class);
+    Route::resource('library.book', BookController::class);
     Route::get('/openlibrary/', OpenLibraryController::class . '@index')->name('openlibrary.index');
     Route::post('/openlibrary/search', OpenLibraryController::class . '@search')->name('openlibrary.search');
 });
