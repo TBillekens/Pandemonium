ef3da5e441e028b9f7d8f2322c56264e4800076d
Timmy Billekens <timmybillekens@gmail.com>
2024-07-07 03:17:28 +0200

    feat: create flash message component

---
+++ resources/js/Components/FlashMessages.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div
+            class="snackbar top active"
+            @click="closeMessage"
+            v-if="flash.message"
+        >
+            <i>done</i>
+            {{ flash.message }}
+        </div>
+        <div
+            class="snackbar top active"
+            @click="closeMessage"
+            v-if="flash.error"
+        >
+            <i>error</i>
+            {{ flash.error }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FlashMessages",
+    computed: {
+        auth() {
+            return this.$page.props.auth;
+        },
+        flash() {
+            return this.$page.props.flash;
+        },
+    },
+    methods: {
+        closeMessage() {
+            this.flash.message = "";
+        },
+        closeError() {
+            this.flash.error = "";
+        },
+    },
+};
+</script>
