d9f3bec1ae126d0cac9eb870db23a9a38d85f2be
Timmy Billekens <timmybillekens@gmail.com>
2024-07-09 18:11:57 +0200

    feat: fixed forgotten registration functionality

--- resources/js/Components/Auth/Register.vue
+++ resources/js/Components/Auth/Register.vue
@@ -3,21 +3,29 @@
         <header>
             <h6>Register</h6>
         </header>
-        <form>
+        <form @submit.prevent="register">
+            <span v-if="failedRegisterError" class="error-text">{{
+                failedRegisterError
+            }}</span>
             <div class="field label round border small-round">
-                <input type="text" lazy />
-                <label>Username</label>
+                <input v-model="name" type="text" lazy />
+                <label>Name</label>
+                <span v-if="nameError" class="error">{{ nameError }}</span>
             </div>
             <div class="field label round border small-round">
-                <input type="email" lazy />
+                <input v-model="email" type="email" lazy />
                 <label>Email</label>
+                <span v-if="emailError" class="error">{{ emailError }}</span>
             </div>
             <div class="field label round border small-round">
-                <input type="password" lazy />
+                <input v-model="password" type="password" lazy />
                 <label>Password</label>
+                <span v-if="passwordError" class="error">{{
+                    passwordError
+                }}</span>
             </div>
             <div class="field label round border small-round">
-                <input type="password" lazy />
+                <input v-model="repeatPassword" type="password" lazy />
                 <label>Repeat Password</label>
             </div>
             <nav class="right-align">
@@ -29,31 +37,101 @@
 
 <script>
 export default {
+    props: {
+        flash: Object,
+        config: Object,
+    },
     data() {
         return {
-            username: "",
+            name: "",
             email: "",
             password: "",
             repeatPassword: "",
+            failedRegisterError: "",
+            nameError: "",
+            passwordError: "",
+            emailError: "",
         };
     },
     computed: {
-        auth() {
-            return this.$page.props.auth;
+        isFormInvalid() {
+            return (
+                !!this.nameError ||
+                !!this.emailError ||
+                !!this.passwordError ||
+                !!this.repeatPasswordError
+            );
         },
     },
     methods: {
+        validateEmail() {
+            if (!this.email) {
+                this.emailError = "Email is required";
+            } else if (!this.email.includes("@")) {
+                this.emailError = "Email is invalid";
+            } else {
+                this.emailError = "";
+            }
+        },
+
+        validateName() {
+            if (!this.name) {
+                this.nameError = "Name is required";
+            } else {
+                this.nameError = "";
+            }
+        },
+
+        validatePassword() {
+            if (!this.password) {
+                this.passwordError = "Password is required";
+            } else {
+                this.passwordError = "";
+            }
+
+            if (this.password !== this.repeatPassword) {
+                this.repeatPasswordError = "Passwords do not match";
+            } else {
+                this.repeatPasswordError = "";
+            }
+
+            if (this.password.length < 6) {
+                this.passwordError = "Password must be at least 6 characters";
+            }
+        },
+
         async register() {
-            try {
-                const response = this.$inertia.post("/register", {
-                    username: this.username,
+            this.validateEmail();
+            this.validateName();
+            this.validatePassword();
+
+            if (this.isFormInvalid) return;
+
+            this.$inertia.post(
+                "auth/register",
+                {
+                    name: this.name,
                     email: this.email,
                     password: this.password,
-                });
-                this.$inertia.visit("/");
-            } catch (error) {
-                console.error("Registration failed:", error);
-            }
+                },
+                {
+                    onError: (error) => {
+                        console.log(error);
+                    },
+                    onSuccess: () => {
+                        this.name = "";
+                        this.email = "";
+                        this.password = "";
+                        this.repeatPassword = "";
+                        this.nameError = "";
+                        this.emailError = "";
+                        this.passwordError = "";
+                        this.repeatPasswordError = "";
+                        this.failedRegisterError = "";
+                        ui("#registerDialog");
+                    },
+                }
+            );
         },
     },
 };
